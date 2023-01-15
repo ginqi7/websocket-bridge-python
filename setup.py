8b21ce3149e01a63de915dcce25eb2e63fa05924
qiqi.jin <qiqi.jin@alibaba-inc.com>
2023-01-15 12:15:37 +0800

    feat: make it as python package.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='websocket_bridge_python',
-    version='0.0.1',
+    version='0.0.2',
     author='qiqi jin',
     author_email='ginqi7@gmail.com',
     url='https://github.com/ginqi7/websocket-bridge-python',
